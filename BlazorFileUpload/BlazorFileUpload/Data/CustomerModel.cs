311491a4d2a8942867b8a6886141563a0fd3cbb4
Kevin Hooper <Kevin.az.hooper@gmail.com>
2023-01-25 14:38:19 -0700

    Added form field submission

---
+++ BlazorFileUpload/BlazorFileUpload/Data/CustomerModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorFileUpload.Data
+{
+    public class CustomerModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; } = "khooper";
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? FileName { get; set; }
+    }
+}
