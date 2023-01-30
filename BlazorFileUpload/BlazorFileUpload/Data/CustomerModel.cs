843a512e0b5085b529837aead38260607332bdc7
Kevin Hooper <Kevin.az.hooper@gmail.com>
2023-01-30 08:47:43 -0700

    UI updates

--- BlazorFileUpload/BlazorFileUpload/Data/CustomerModel.cs
+++ BlazorFileUpload/BlazorFileUpload/Data/CustomerModel.cs
@@ -1,11 +1,18 @@
-﻿namespace BlazorFileUpload.Data
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorFileUpload.Data
 {
     public class CustomerModel
     {
         public int Id { get; set; }
         public string UserName { get; set; } = "khooper";
-        public string? FirstName { get; set; }
-        public string? LastName { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
         public string? FileName { get; set; }
     }
 }
